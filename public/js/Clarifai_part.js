c226acf8d58de93745e16b342b088dc81233cc82
bitnami <bitnami@ubuntu.defaultdomain>
2016-03-05 06:06:26 +0000

    got tags working

--- public/js/Clarifai_part.js
+++ public/js/Clarifai_part.js
@@ -6,26 +6,28 @@ var CLARIFAI_SECRET = "T_eDYqVrqeEHoP5ZgD99BxX2ybjOIoxTWmAxboq8";
 Clarifai.initAPI(CLARIFAI_ID, CLARIFAI_SECRET);
 var tags = [];
 
-function getTags() {
-	var imageURL = '';//url of the image that was uploaded
-	var imageId = "uploadedImage";
-	Clarifai.tagURL( imageURL , imageId, resultHandler );
-}
-
 function resultHandler( err, res ) {
 	if( err != null ) {
 		console.log(err);
 	}else {
-		if( opts["print-results"] ) {
-			//If tags exist they will be pushed into the tags array
-			if( typeof res["status_code"] === "string" &&( res["status_code"] === "OK" || res["status_code"] === "PARTIAL_ERROR" )) {
-				for( i = 0; i < res.results.length; i++ ) {
-					if( res["results"][i]["status_code"] === "OK" ) {
-						tags.push(res["results"][i].result["tag"]["classes"]);
-					}
+		//If tags exist they will be pushed into the tags array
+		if( typeof res["status_code"] === "string" &&( res["status_code"] === "OK" || res["status_code"] === "PARTIAL_ERROR" )) {
+			for( i = 0; i < res.results.length; i++ ) {
+				if( res["results"][i]["status_code"] === "OK" ) {
+					tags.push(res["results"][i].result["tag"]["classes"]);
 				}
 			}
 		}
 	}
 }
+
+
+function getTags() {
+	var imageURL = 'http://i.imgur.com/b7mzeLI.jpg';
+	var imageId = "uploadedImage";
+	Clarifai.tagURL( imageURL , imageId, resultHandler );
+	console.log("test");
+}
+
+getTags()
 
