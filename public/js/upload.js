38fdf01be56ce19f4e726afb4cc6af72df483375
Andres E Moser <amose001@fiu.edu>
2016-03-05 02:24:40 -0500

    update

--- public/js/upload.js
+++ public/js/upload.js
@@ -5,9 +5,39 @@ function readURL(input) {
     reader.onload = function (e) {
       $('#blah')
       .attr('src', e.target.result)
-      .width(300)
-      .height(200);
+      // var orientation = get_orientation(e.target.result)
+      // if(orientation == 'landscape') {
+        // .width(640)
+        // .height(360);
+      // }
+    //   else if (orientation == 'portrait') {
+        .width(360)
+        .height(640);
+    //   }
+    //   else {
+    //     .width(640)
+    //     .height(640);
+    //   }
     };
     reader.readAsDataURL(input.files[0]);
   }
+}
+
+function get_orientation(src){
+
+  img = new Image();
+  img.src = src;
+  var width = img.width;
+  var height = img.height;
+  height = height + height // Double the height to get best
+  //height = height + (height / 2) // Increase height by 50%
+
+  if(width > height) {
+    return "landscape";
+  } else if(height > width){
+    return "portrait";
+  } else {
+    return "square";
+  }
+
 }
