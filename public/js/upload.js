7fff67f812707faef7837770c6d2d01b169cbc0b
Andres E Moser <amose001@fiu.edu>
2016-03-05 03:01:23 -0500

    fixed preview adapting for protrait/landscape

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,34 +1,28 @@
-function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
+function previewFile() {
+  var preview = document.querySelector('img');
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
 
-    reader.onload = function (e) {
-      $('#blah')
-      .attr('src', e.target.result)
+  reader.addEventListener("load", function () {
+    preview.src = reader.result;
+  }, false);
 
-      .width(360)
-      .height(640);
-    };
-    reader.readAsDataURL(input.files[0]);
+  if (file) {
+    reader.readAsDataURL(file);
   }
 }
-/*
-function get_orientation(src){
-
-  img = new Image();
-  img.src = src;
-  var width = img.width;
-  var height = img.height;
-  height = height + height // Double the height to get best
-  //height = height + (height / 2) // Increase height by 50%
-
-  if(width > height) {
-    return "landscape";
-  } else if(height > width){
-    return "portrait";
-  } else {
-    return "square";
-  }
-
-}
-*/
+/* old non functioning code */
+// function readURL(input) {
+//   if (input.files && input.files[0]) {
+//     var reader = new FileReader();
+//
+//     reader.onload = function (e) {
+//       $('#blah')
+//       .attr('src', e.target.result)
+//
+//       .width(360)
+//       .height(640);
+//     };
+//     reader.readAsDataURL(input.files[0]);
+//   }
+// }
