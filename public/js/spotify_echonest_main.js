663d07c40b81be3b5d5b8888e1373eecdfb213ff
leonliang33 <llian008@fiu.edu>
2016-03-05 10:39:33 -0500

    try it now

--- public/js/spotify_echonest_main.js
+++ public/js/spotify_echonest_main.js
@@ -49,7 +49,7 @@ function getEchonestTags() {
         console.log("got out of loop\n");
 	//	spotifyPlaylist();
     }).done(function () {
-        spotifyPlaylist();
+        //spotifyPlaylist();
     });
     console.log("I got here, out of the functions\n");
     // var myFunction = setTimeout(spotifyPlaylist, 2000);
